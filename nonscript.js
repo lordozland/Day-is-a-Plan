beb82257a1700afb6ef2d90cca958660d73de963
William Crews <crewswh@gmail.com>
2021-08-07 14:18:21 -0400

    abt to change

---
+++ nonscript.js
@@ -0,0 +1,10 @@
+var node = document.createElement("LI");                 // Create a <li> node
+var textnode = document.createTextNode("Water");         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+
+
+document.getElementById("lulz").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+var lulz = document.getElementById("lulz")
+var notegive = document.getElementById("notegive")
+
